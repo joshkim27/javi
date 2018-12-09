e6f420ad5ed7124201a3eb77afcaf71aac3793de
kevin.eom <kevn.eom@balancehero.com>
2018-12-09 16:42:47 +0530

    get_monthly_report 적용

--- app.py
+++ app.py
@@ -1344,7 +1344,7 @@ def put_daily2(userId, uimDailySales, uimDailyBuying, todayDate):
 def get_this_montly_report(userId):
     now = datetime.now()
     return jsonify({
-        "messages":[{"text": get_montly_report(userId,now)}]
+        "messages":[{"text": get_monthly_report(userId,now)}]
     })
 
 
@@ -1353,103 +1353,9 @@ def get_previous_montly_report(userId):
     now = datetime.now() - relativedelta(months=1)
     now = now.replace(day=(monthrange(int(now.strftime('%Y')), int(now.strftime('%m')))[1]))
     return jsonify({
-        "messages":[{"text": get_montly_report(userId,now)}]
+        "messages":[{"text": get_monthly_report(userId,now)}]
     })
 
-
-def get_montly_report(userId, now):
-
-    today = now.strftime('%Y%m%d')
-    this_month = now.strftime(('%Y%m'))
-    this_month_text = now.strftime('%b')
-    start_date_month = this_month + '01'
-
-    dates = monthcalendar(int(now.strftime('%Y')), int(now.strftime('%m')))
-    message = now.strftime('%b') + ' Sales Report\n' + add_postfix(start_date_month[6:]) + \
-              ' ' + this_month_text + ' ~ ' + add_postfix(today[6:8]) + ' ' + this_month_text + '\n' +\
-              '\nWeek \n : Buying | Sales | Profit\n'
-
-    daily_table = dynamodb.Table(DAILY_TABLE)
-
-    fe = Key('userDailyId').between(userId + start_date_month, userId + today)
-    response = daily_table.scan(
-        FilterExpression=fe,
-    )
-    temp = {}
-    for i in response['Items']:
-        logger.debug(i)
-        temp[i['userDailyId']] = [i['uimDailySales'], i['uimDailyBuying']]
-
-    logger.debug(temp)
-
-    total_sales = 0
-    total_buying = 0
-
-    for week_days in dates:
-        logger.debug(week_days)
-        sum_sales = 0
-        sum_buying = 0
-        lastday_of_week = 0
-        uncountable_days = 0
-
-
-        for day in week_days :
-            logger.debug(day)
-            if day == 0:
-                uncountable_days = uncountable_days + 1
-            elif day < 10:
-                daily_id = userId + this_month + '0' + str(day)
-                lastday_of_week = day
-            else:
-                daily_id = userId + this_month + str(day)
-                lastday_of_week = day
-
-            if day > 0 and (daily_id in temp):
-
-                add_sales = temp[daily_id][0]
-                add_buying = temp[daily_id][1]
-            else:
-                add_sales = 0
-                add_buying = 0
-
-            sum_sales = sum_sales + add_sales
-            sum_buying = sum_buying + add_buying
-
-        text_week = 'W' + (now.replace(day=lastday_of_week)).strftime('%W')
-        if uncountable_days > 0 :
-            text_week = text_week + '(%ddays)'%(7-uncountable_days)
-
-        text_week = text_week + '\n'
-
-        message = message + text_week + ' :%6d | %6d | %6d'%( sum_sales, sum_buying, (sum_sales - sum_buying)) + '\n'
-
-        total_sales = total_sales + sum_sales
-        total_buying = total_buying + sum_buying
-
-        logger.debug(message)
-    message = message + '\n' + this_month_text + '\n' + \
-              ' :%6d | %6d | %6d'%(total_sales, total_buying, (total_sales - total_buying)) + \
-              '\n\nNet Profit\n= Profit - Monthly Cost\n\n'
-    monthly_table = dynamodb.Table(MONTHLY_TABLE)
-
-    fe = Key('userMonthlyId').eq(userId + this_month)
-    res = monthly_table.scan(
-        FilterExpression=fe,
-    )
-
-    logger.debug(res)
-
-    item_monthly = res.get('Items')
-    if not item_monthly:
-        total_cost = 0
-    else:
-        total_cost = item_monthly[0].get('uioRentalAmount') + item_monthly[0].get('uioEmployeeAmount') + item_monthly[0].get('uioOtherCost')
-
-    message = message + '%drs\n= %d - %d'%((total_sales - total_buying - total_cost),(total_sales - total_buying), total_cost )
-
-    logger.debug(message)
-
-    return message
 
 @app.route("/daily/<string:update_date>", methods=['POST'])
 def update_daily(update_date):
